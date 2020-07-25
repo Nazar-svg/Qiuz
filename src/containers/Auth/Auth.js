cef031667de274e6bd780e38244ab53d3c184d3c
Nazar <nazar.kolos90@gmail.com>
2020-07-25 21:33:03 +0300

    Working in avtorization

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
     state = {
@@ -20,7 +21,7 @@ export default class Auth extends Component {
                     email: true
                 }
             },
-            pasvord: {
+            password: {
                 value: '',
                 type: 'pasword',
                 label: 'Пароль',
@@ -35,11 +36,32 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControl.email.value,
+            password: this.state.formControl.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAPHMV3caQjZbhqx3LtaE30kZ8hgZhtNts', authData)
+            console.log(response.data)
+        }catch (e){
+          console.log(e)
+        }
     }
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControl.email.value,
+            password: this.state.formControl.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAPHMV3caQjZbhqx3LtaE30kZ8hgZhtNts', authData)
+            console.log(response.data)
+        }catch (e){
+          console.log(e)
+        }
+      
     }
 
     submitHandler = event => {
